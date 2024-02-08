a5c0326d814c598dcc2778e2d628dbd80b9d725f
ABDALLAHAADAM11 <149729863+ABDALLAHAADAM11@users.noreply.github.com>
2024-02-08 13:00:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+    function validation (password, email){
+        if (email == "abdallahadam@gmail.com" || password == "2200704272@2024")
+        console.log(`User email is ${email}, login successful`)
+        else 
+        console.log(`incorrect user credentials`)
+        }
+        
+        validation("2200704272@2024","abdallahadam@gmail.com");
+
